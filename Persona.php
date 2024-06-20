c5002740dae5981e359a45aa280bcaf33a77a6c6
Rocio-Ayelen-Fernandez <150625389+Rocio-Ayelen-Fernandez@users.noreply.github.com>
2024-06-20 17:08:49 -0300

    Update Persona.php

--- Persona.php
+++ Persona.php
@@ -1,72 +1,84 @@
 <?php
-include_once "BaseDatos.php";
+include_once 'BaseDatos.php';
 
-class Persona {
+class Persona
+{
     private $nrodoc;
     private $nombre;
     private $apellido;
     private $telefono;
     private $mensajeoperacion;
 
-    public function __construct(){
-        $this->nrodoc = "";
-        $this->nombre = "";
-        $this->apellido = "";
-        $this->telefono = "";
+    public function __construct()
+    {
+        $this->nrodoc = '';
+        $this->nombre = '';
+        $this->apellido = '';
+        $this->telefono = '';
     }
 
-  //  public function cargar($nrodoc, $nomb, $ape, $tel){
-        public function cargar($param){
-
+    public function cargar($param)
+    {
         $this->setNrodoc($param['nrodoc']);
         $this->setNombre($param['nomb']);
         $this->setApellido($param['ape']);
         $this->setTelefono($param['tel']);
     }
 
-    public function setNrodoc($NroDNI){
+    public function setNrodoc($NroDNI)
+    {
         $this->nrodoc = $NroDNI;
     }
 
-    public function setNombre($Nom){
+    public function setNombre($Nom)
+    {
         $this->nombre = $Nom;
     }
 
-    public function setApellido($Ape){
+    public function setApellido($Ape)
+    {
         $this->apellido = $Ape;
     }
 
-    public function setTelefono($tel){
+    public function setTelefono($tel)
+    {
         $this->telefono = $tel;
     }
 
-    public function setmensajeoperacion($mensajeoperacion){
+    public function setmensajeoperacion($mensajeoperacion)
+    {
         $this->mensajeoperacion = $mensajeoperacion;
     }
 
-    public function getNrodoc(){
+    public function getNrodoc()
+    {
         return $this->nrodoc;
     }
 
-    public function getNombre(){
+    public function getNombre()
+    {
         return $this->nombre;
     }
 
-    public function getApellido(){
+    public function getApellido()
+    {
         return $this->apellido;
     }
 
-    public function getTelefono(){
+    public function getTelefono()
+    {
         return $this->telefono;
     }
 
-    public function getmensajeoperacion(){
+    public function getmensajeoperacion()
+    {
         return $this->mensajeoperacion;
     }
 
-    public function Buscar($dni){
+    public function Buscar($dni)
+    {
         $base = new BaseDatos();
-        $consultaPersona = "SELECT * FROM persona WHERE nrodoc = " . $dni;
+        $consultaPersona = 'SELECT * FROM persona WHERE nrodoc = ' . $dni;
         $resp = false;
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consultaPersona)) {
@@ -86,27 +98,31 @@ class Persona {
         return $resp;
     }
 
-
-
     //a este metodo le sacamos la palabra static
-    public function listar($condicion = ""){
+    public function listar($condicion = '')
+    {
         $arregloPersona = null;
         $base = new BaseDatos();
-        $consultaPersonas = "SELECT * FROM persona ";
-        if ($condicion != "") {
+        $consultaPersonas = 'SELECT * FROM persona ';
+        if ($condicion != '') {
             $consultaPersonas = $consultaPersonas . ' WHERE ' . $condicion;
         }
-        $consultaPersonas .= " ORDER BY apellido ";
+        $consultaPersonas .= ' ORDER BY apellido ';
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consultaPersonas)) {
-                $arregloPersona = array();
+                $arregloPersona = [];
                 while ($row2 = $base->Registro()) {
                     $NroDoc = $row2['nrodoc'];
                     $Nombre = $row2['nombre'];
                     $Apellido = $row2['apellido'];
                     $Telefono = $row2['telefono'];
                     $perso = new Persona();
-                    $perso->cargar(['nomb'=>$Nombre, 'nrodoc' => $NroDoc, 'ape' => $Apellido, 'tel' => $Telefono]);
+                    $perso->cargar([
+                        'nomb' => $Nombre,
+                        'nrodoc' => $NroDoc,
+                        'ape' => $Apellido,
+                        'tel' => $Telefono,
+                    ]);
                     array_push($arregloPersona, $perso);
                 }
             } else {
@@ -118,10 +134,20 @@ class Persona {
         return $arregloPersona;
     }
 
-    public function insertar(){
+    public function insertar()
+    {
         $base = new BaseDatos();
         $resp = false;
-        $consultaInsertar = "INSERT INTO persona (nrodoc, apellido, nombre, telefono) VALUES (" . $this->getNrodoc() . ",'" . $this->getApellido() . "','" . $this->getNombre() . "','" . $this->getTelefono() . "')";
+        $consultaInsertar =
+            'INSERT INTO persona (nrodoc, apellido, nombre, telefono) VALUES (' .
+            $this->getNrodoc() .
+            ",'" .
+            $this->getApellido() .
+            "','" .
+            $this->getNombre() .
+            "','" .
+            $this->getTelefono() .
+            "')";
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consultaInsertar)) {
                 $resp = true;
@@ -134,10 +160,19 @@ class Persona {
         return $resp;
     }
 
-    public function modificar(){
+    public function modificar()
+    {
         $resp = false;
         $base = new BaseDatos();
-        $consultaModifica = "UPDATE persona SET apellido='" . $this->getApellido() . "', nombre='" . $this->getNombre() . "', telefono='" . $this->getTelefono() . "' WHERE nrodoc=" . $this->getNrodoc();
+        $consultaModifica =
+            "UPDATE persona SET apellido='" .
+            $this->getApellido() .
+            "', nombre='" .
+            $this->getNombre() .
+            "', telefono='" .
+            $this->getTelefono() .
+            "' WHERE nrodoc=" .
+            $this->getNrodoc();
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consultaModifica)) {
                 $resp = true;
@@ -150,11 +185,13 @@ class Persona {
         return $resp;
     }
 
-    public function eliminar(){
+    public function eliminar()
+    {
         $base = new BaseDatos();
         $resp = false;
         if ($base->Iniciar()) {
-            $consultaBorra = "DELETE FROM persona WHERE nrodoc=" . $this->getNrodoc();
+            $consultaBorra =
+                'DELETE FROM persona WHERE nrodoc=' . $this->getNrodoc();
             if ($base->Ejecutar($consultaBorra)) {
                 $resp = true;
             } else {
@@ -166,8 +203,17 @@ class Persona {
         return $resp;
     }
 
-    public function __toString(){
-        return "DNI: " . $this->getNrodoc() . "\nNombre: " . $this->getNombre() . "\nApellido: " . $this->getApellido() . "\ntelefono: " . $this->getTelefono() . "\n";
+    public function __toString()
+    {
+        return 'DNI: ' .
+            $this->getNrodoc() .
+            "\nNombre: " .
+            $this->getNombre() .
+            "\nApellido: " .
+            $this->getApellido() .
+            "\ntelefono: " .
+            $this->getTelefono() .
+            "\n";
     }
 }
 ?>
