0d2b74b41f7ca7137a74981de6bcd2d3c0b5b4fa
Rocio-Ayelen-Fernandez <150625389+Rocio-Ayelen-Fernandez@users.noreply.github.com>
2024-06-20 17:07:48 -0300

    Update Empresa.php

--- Empresa.php
+++ Empresa.php
@@ -1,62 +1,74 @@
 <?php
-class Empresa {
-    private $idempresa;//SQL
-    private $enombre;//SQL
-    private $edireccion;//SQL
-    private $colViaje; 
+class Empresa
+{
+    private $idempresa; //SQL
+    private $enombre; //SQL
+    private $edireccion; //SQL
+    private $colViaje;
     private $mensajeoperacion;
 
-    public function __construct(){
-        $this->idempresa = "";
-        $this->enombre = "";
-        $this->edireccion = "";
+    public function __construct()
+    {
+        $this->idempresa = '';
+        $this->enombre = '';
+        $this->edireccion = '';
         $this->colViaje = [];
     }
 
-    
-    public function setIdempresa($idemp){
+    public function setIdempresa($idemp)
+    {
         $this->idempresa = $idemp;
     }
 
-    public function setEnombre($nom){
+    public function setEnombre($nom)
+    {
         $this->enombre = $nom;
     }
 
-    public function setEdireccion($dir){
+    public function setEdireccion($dir)
+    {
         $this->edireccion = $dir;
     }
 
-    public function setMensajeOperacion($mensajeoperacion){
+    public function setMensajeOperacion($mensajeoperacion)
+    {
         $this->mensajeoperacion = $mensajeoperacion;
     }
 
-    public function setColViaje($viaje){
+    public function setColViaje($viaje)
+    {
         $this->colViaje = $viaje;
     }
 
-    public function getIdempresa(){
+    public function getIdempresa()
+    {
         return $this->idempresa;
     }
 
-    public function getEnombre(){
+    public function getEnombre()
+    {
         return $this->enombre;
     }
 
-    public function getEdireccion(){
+    public function getEdireccion()
+    {
         return $this->edireccion;
     }
 
-    public function getMensajeOperacion(){
+    public function getMensajeOperacion()
+    {
         return $this->mensajeoperacion;
     }
 
-    public function getColViaje(){
+    public function getColViaje()
+    {
         return $this->colViaje;
     }
 
-    public function buscar($idemp){
+    public function buscar($idemp)
+    {
         $base = new BaseDatos();
-        $consultaEmpresa = "SELECT * FROM empresa WHERE idempresa = " . $idemp;
+        $consultaEmpresa = 'SELECT * FROM empresa WHERE idempresa = ' . $idemp;
         $resp = false;
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consultaEmpresa)) {
@@ -75,22 +87,23 @@ class Empresa {
         return $resp;
     }
 
-    public function cargar($idemp, $nom, $dir, $colViajes){
+    public function cargar($idemp, $nom, $dir, $colViajes)
+    {
         $this->setIdempresa($idemp);
         $this->setEnombre($nom);
         $this->setEdireccion($dir);
         $this->setColViaje($colViajes);
     }
 
-
-    public function listar($condicion = ""){
+    public function listar($condicion = '')
+    {
         $arregloEmpresa = null;
         $base = new BaseDatos();
-        $consultaEmpresas = "SELECT * FROM empresa ";
-        if ($condicion != "") {
+        $consultaEmpresas = 'SELECT * FROM empresa ';
+        if ($condicion != '') {
             $consultaEmpresas = $consultaEmpresas . ' WHERE ' . $condicion;
         }
-        $consultaEmpresas .= " ORDER BY idempresa ";
+        $consultaEmpresas .= ' ORDER BY idempresa ';
 
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consultaEmpresas)) {
@@ -100,7 +113,7 @@ class Empresa {
                     $nom = $row['enombre'];
                     $dir = $row['edireccion'];
                     $empresa = new Empresa();
-
+                    // $empresa->buscar($idemp);
                     $colViajes = $this->getColViaje();
                     $empresa->cargar($idemp, $nom, $dir, $colViajes);
                     array_push($arregloEmpresa, $empresa);
@@ -114,18 +127,18 @@ class Empresa {
         return $arregloEmpresa;
     }
 
-
-
-
-
-
-    public function insertar(){
+    public function insertar()
+    {
         $base = new BaseDatos();
         $resp = false;
-        $consultaInsertar = "INSERT INTO empresa (idempresa, enombre, edireccion) VALUES ('" . $this->getIdempresa()."','".$this->getEnombre() . "','" . $this->getEdireccion() . "')";
+        $consultaInsertar =
+            "INSERT INTO empresa (enombre, edireccion) VALUES ('" .
+            $this->getEnombre() .
+            "','" .
+            $this->getEdireccion() .
+            "')";
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consultaInsertar)) {
-                // $this->setIdempresa($base->devuelveIDInsercion($consultaInsertar));
                 $resp = true;
             } else {
                 $this->setMensajeOperacion($base->getError());
@@ -136,14 +149,17 @@ class Empresa {
         return $resp;
     }
 
-
-
-
-
-    public function modificar(){
+    public function modificar()
+    {
         $resp = false;
         $base = new BaseDatos();
-        $consultaModifica = "UPDATE empresa SET enombre='" . $this->getEnombre() . "', edireccion='" . $this->getEdireccion() . "' WHERE idempresa=" . $this->getIdempresa();
+        $consultaModifica =
+            "UPDATE empresa SET enombre='" .
+            $this->getEnombre() .
+            "', edireccion='" .
+            $this->getEdireccion() .
+            "' WHERE idempresa=" .
+            $this->getIdempresa();
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consultaModifica)) {
                 $resp = true;
@@ -156,11 +172,13 @@ class Empresa {
         return $resp;
     }
 
-    public function eliminar(){
+    public function eliminar()
+    {
         $base = new BaseDatos();
         $resp = false;
         if ($base->Iniciar()) {
-            $consultaBorrar = "DELETE FROM empresa WHERE idempresa=" . $this->getIdempresa();
+            $consultaBorrar =
+                'DELETE FROM empresa WHERE idempresa=' . $this->getIdempresa();
             if ($base->Ejecutar($consultaBorrar)) {
                 $resp = true;
             } else {
@@ -172,14 +190,30 @@ class Empresa {
         return $resp;
     }
 
-    public function cadenaViajes(){
-        $cadena = "";
-        foreach ($this->getColViaje() as $viaje) {
-            $cadena .= $viaje ."\n";
+    public function cadenaViajes()
+    {
+        $cadena = '';
+
+        if (count($this->getColViaje()) < 1) {
+            foreach ($this->getColViaje() as $viaje) {
+                $cadena .= $viaje . "\n";
+            }
+        } else {
+            $cadena = "No se encuentran Viajes en la Empresa\n";
         }
+        return $cadena;
     }
 
-    public function __toString(){
-        return "ID Empresa: " . $this->getIdempresa() . "\nNombre: " . $this->getEnombre() . "\nDirección: " . $this->getEdireccion() . "\nViajes:\n". $this->cadenaViajes()."\n";
+    public function __toString()
+    {
+        return 'ID Empresa: ' .
+            $this->getIdempresa() .
+            "\nNombre: " .
+            $this->getEnombre() .
+            "\nDirección: " .
+            $this->getEdireccion() .
+            "\nViajes:\n" .
+            $this->cadenaViajes() .
+            "\n";
     }
 }
